221ff089e15654d9ddfc7dc1709307dcc63a7086
Fernando <fernando@computoma.com>
2026-09-09 17:47:07 +0100

    Add a new macOS host named `macstudio-m1ultra`

---
+++ hosts/macstudio-m1ultra/install-homebrew-apps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env /bin/bash
+# shellcheck disable=SC2155
+
+set -Eeuo pipefail
+
+readonly SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+readonly COMMON_DIR="$(cd "$SCRIPT_DIR/../common" && pwd)"
+readonly ROOT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
+readonly VERBOSE="${VERBOSE:-0}"
+readonly HOMEBREW_EXTRA_CASKS_HOST=(utm)
+export HOMEBREW_NO_ASK=1
+
+source "$SCRIPT_DIR/env.sh"
+source "$ROOT_DIR/etc/scripts/utils.sh"
+source "$COMMON_DIR/env.sh"
+
+# NOTE: Executing the `brewª command without the `run` harness because it has a
+# rich TUI.
+
+logi "Installing Homebrew's formulae ..."
+brew install "${HOMEBREW_DEFAULT_FORMULAE[@]}"
+brew install --ignore-dependencies liblinear lua nmap
+brew unlink openssl@3
+
+logi "Installing Homebrew's casks ..."
+brew install --casks "${HOMEBREW_DEFAULT_CASKS[@]}" \
+	"${HOMEBREW_EXTRA_CASKS_HOST[@]}"
