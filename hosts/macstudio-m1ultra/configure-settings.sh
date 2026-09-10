80fbac569716aec626fa820937dc68f832e259e5
Fernando <fernando@computoma.com>
2026-09-10 22:40:51 +0100

    Update macstudio-m1ultra's app settings

--- hosts/macstudio-m1ultra/configure-settings.sh
+++ hosts/macstudio-m1ultra/configure-settings.sh
@@ -10,8 +10,8 @@ VERBOSE="${VERBOSE:-0}"
 source "$SCRIPT_DIR/env.sh"
 source "$ROOT_DIR/etc/scripts/utils.sh"
 
-# readonly ACTMON_KEY="com.apple.ActivityMonitor"
-# readonly ACTMON_FILE="$SCRIPT_DIR/etc/${ACTMON_KEY}.plist"
+readonly ACTMON_KEY="com.apple.ActivityMonitor"
+readonly ACTMON_FILE="$SCRIPT_DIR/etc/${ACTMON_KEY}.plist"
 readonly ALTTAB_KEY="com.lwouis.alt-tab-macos"
 readonly ALTTAB_FILE="$ROOT_DIR/etc/macos/${ALTTAB_KEY}.plist"
 readonly MACMOUSEFIX_KEY="com.nuebling.mac-mouse-fix"
@@ -34,7 +34,7 @@ parse_input_args () {
 
 install_app_settings () {
 	logi "Installing applications settings ..."
-	# run defaults import "$ACTMON_KEY" "$ACTMON_FILE"
+	run defaults import "$ACTMON_KEY" "$ACTMON_FILE"
 	run defaults import "$ALTTAB_KEY" "$ALTTAB_FILE"
 	run cp "$MACMOUSEFIX_FILE" "$MACMOUSEFIX_FILE_SOURCE"
 	run rm -rf "$OBS_DIR_SOURCE"/*
@@ -45,7 +45,7 @@ install_app_settings () {
 save_app_settings () {
 	logi "Exporting applications settings ..."
 	run mkdir -p "$SCRIPT_DIR/etc"
-	# run defaults export "$ACTMON_KEY" "$ACTMON_FILE"
+	run defaults export "$ACTMON_KEY" "$ACTMON_FILE"
 	run defaults export "$ALTTAB_KEY" "$ALTTAB_FILE"
 	run cp "$MACMOUSEFIX_FILE_SOURCE" "$MACMOUSEFIX_FILE"
 	run rm -rf "$OBS_DIR"
