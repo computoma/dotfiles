733f5cb4044286cdc4efdceb2ef54ab0ec287eeb
Fernando <fernando@computoma.com>
2026-09-12 07:34:13 +0100

    Fix typo in macvdm install script

--- etc/macos/scripts/install-macvdmtool.sh
+++ etc/macos/scripts/install-macvdmtool.sh
@@ -93,7 +93,7 @@ trap 'cleanup' EXIT
 parse_input_args "$@"
 check_preconds
 if [[
-	-d "$macvdm_git_dir/macvmd" &&
+	-d "$macvdm_git_dir/macvdm" &&
 	$(run git -C "$macvdm_git_dir/macvdmtool" rev-parse --is-inside-work-tree) == "true"
 ]]; then
 	update_macvdm
