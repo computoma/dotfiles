34fadfcecd767dbf4552f4ec835f712719b57d26
Fernando <fernando@computoma.com>
2026-09-19 13:58:00 +0100

    Remove visualdiffer from `HOMEBREW_DEFAULT_CASKS`

--- hosts/common/env.sh
+++ hosts/common/env.sh
@@ -10,7 +10,7 @@ export HOMEBREW_DEFAULT_FORMULAE=(
 export HOMEBREW_DEFAULT_CASKS=(
 	alt-tab brave-browser dbeaver-community font-jetbrains-mono-nerd-font fork
 	geekbench ghostty iina mac-mouse-fix obs transmission visual-studio-code
-	visualdiffer zed
+	zed
 )
 
 export HOMEBREW_EXTRA_CASKS_LAPTOP=(
